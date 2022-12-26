41f54e04b1bc5903472354e74b4923a3caf68fc1
rai <mj82xx@gmail.com>
2022-12-26 22:30:38 +0900

    fix: basePath

--- next.config.js
+++ next.config.js
@@ -2,4 +2,6 @@ const withExportImages = require("next-export-optimize-images");
 
 module.exports = withExportImages({
   reactStrictMode: true,
+  basePath: process.env.GITHUB_ACTIONS ? "/nextjs-tutorial-blog" : "",
+  trailingSlash: true,
 });
